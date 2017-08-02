27de9106f498c8b6a384d67ec2ce38e221fa6ee3
qtotuan <quynh.totuan@gmail.com>
2017-08-02 19:38:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import createStore from './createStore'
+import changeCount from './reducers/changeCount'
 
 export function render() {
   ReactDOM.render(
-    <App />,
+    <App store={store}/>,
     document.getElementById('root')
   );
 };
 
-render();
+const store = createStore(changeCount)
+
+store.dispatch({ type: '@@INIT'})
+
+// render();
